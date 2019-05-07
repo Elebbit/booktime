c85244166c1446dbdd48746572b56bb773acdc99
De.Elebit <de.elebit@gmail.com>
2019-05-07 21:29:28 +0900

    ~35% : Below Creating the Basket Functionality

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
-from main import views
+from django.contrib.auth import views as auth_views
+from main import views, forms
 from main import models
 
 urlpatterns = [
+    path('adress/', views.AddressListView.as_view(), name='address_list'),
+    path('adress/create/', views.AddressCreateView.as_view(), name='address_create'),
+    path('adress/<int:pk>/', views.AddressUpdateView.as_view(), name='address_update'),
+    path('adress/<int:pk>/delete/', views.AddressDeleteView.as_view(), name='address_delete'),
+    path('login/',
+         auth_views.LoginView.as_view(template_name='login.html', form_class=forms.AuthenticationForm,),
+         name='login',
+         ),
     path('signup/', views.SignupView.as_view(), name="signup"),
     path("product/<slug:slug>/", DetailView.as_view(model=models.Product), name="product"),
     path("products/<slug:tag>/", views.ProductListView.as_view(), name="products",),
